a6a4cbc2704a53a2a0d41a9935b69e872f8fccce
WizzMike <163504593+WizzMike@users.noreply.github.com>
2024-04-11 00:04:24 -0700

    Add files via upload
    This is the form user used to create his or her account (Create.php) and Login form that help user to log in .

---
+++ create.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home | Account Page</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="header"><h2>Create a New Account Here</h2></div>
+		<div class="content">
+			<form method="post" action="">
+				<ul>
+					<li><label>Trainers Names</label></li>
+					<li><input type="text" name="tname"></li>
+					<li><label>User Name</label></li>
+					<li><input type="text" name="uname"></li>
+					<li><label>Password</label></li>
+					<li><input type="password" name="password"></li>
+					<li><button name="btn">Create</button></li>
+					<li><a href="login.php">Already have an Account? Log in Here</a></li>
+					<li><label>
+						<?php
+                        $connect= mysqli_connect("localhost","root","","group2");
+						if (isset($_POST["btn"])) {
+						$tname = $_POST["tname"];
+						$uname = $_POST["uname"];
+						$password = $_POST["password"];
+
+						$insert= "INSERT INTO student (trainername,username,password) VALUES('$tname','$uname','$password')";
+						$query= mysqli_query($connect,$insert);
+
+						}
+					
+						 ?>
+						
+					</li>
+
+				</ul>
+			</form>
+		</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home| Login Page</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="container">
+	<div class="header">
+		<h2>Login Here</h2>
+	</div>
+	<div class="content">
+		<form method="post" action="">
+			<ul>
+				<li><label>User Name:</label></li>
+				<li><input type="text" name="uname"></li>
+				<li><label>Password</label></li>
+				<li><input type="Password" name="password"></li>
+				<li><button name="btn">Log in</button></li>
+				<li><a href="create.php">Have no account? Create Account</a></li>
+				<li><label>
+					<?php
+					if (isset($_POST["btn"])) {
+					$connect= mysqli_connect("localhost","root","","group2");
+					$username = $_POST["uname"];
+					$password = $_POST["password"];
+				
+					$result = mysqli_query($connect,"SELECT * FROM student WHERE username='$username' AND password='$password'");
+					if (mysqli_num_rows($result) ==1) {
+						$_SESSION['mysession']=$username;
+						header("location:homepage.php");
+					}else{
+						echo "Wrong User name or Password";
+					}
+				}
+				?>
+				</label></li>
+			</ul>
+		</form>
+	</div>
+</div>
+</body>
+</html>
